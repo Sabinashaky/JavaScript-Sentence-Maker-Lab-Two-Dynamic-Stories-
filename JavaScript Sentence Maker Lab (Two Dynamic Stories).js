76d23519ac09e869fdc26e38c235144bd282dc41
sabina shakya <sabinashakya099@gmail.com>
2026-05-24 22:00:35 +0545

    Add files via upload

---
+++ JavaScript Sentence Maker Lab (Two Dynamic Stories).js
@@ -0,0 +1,21 @@
+let adjective = "fierce";
+let noun = "dragon";
+let verb = "sleeping";
+let place = "mountain";
+let adjective2 = "smoky";
+let noun2 = "pizza";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+adjective = "brave";
+noun = "knight";
+verb = "running";
+place = "castle";
+adjective2 = "golden";
+noun2 = "apples";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
+/*
+*/
